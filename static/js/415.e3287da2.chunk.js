5dd13218ccb2fa26155cf71b67dfb5d13fa582cf
nickleso <103372501+nickleso@users.noreply.github.com>
2023-01-06 14:18:49 +0000

    Deploying to gh-pages from @ nickleso/phonebook-app@0c1b3459a3dd83f7b0b130605b781cb1fbd798b4 🚀

---
+++ static/js/415.e3287da2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkphonebook_app=self.webpackChunkphonebook_app||[]).push([[415],{415:function(e,s,n){n.r(s);var o=n(184);s.default=function(){return(0,o.jsx)("main",{children:(0,o.jsx)("section",{className:"section",children:(0,o.jsxs)("div",{className:"container home",children:[(0,o.jsx)("h1",{className:"home__title",children:"Welcome to Phonebook!"}),(0,o.jsx)("div",{className:"home__wrap"})]})})})}}}]);
+//# sourceMappingURL=415.e3287da2.chunk.js.map
